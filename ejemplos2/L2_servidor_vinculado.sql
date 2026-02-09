c8eff0480476d7114428a28910d4597a613f73d8
aldebarran22 <antonioespinherranz@hotmail.com>
2026-02-09 12:36:49 +0100

    12

---
+++ ejemplos2/L2_servidor_vinculado.sql
@@ -0,0 +1,40 @@
+
+
+-- Ejemplo de un servidor vinculado al servidor local de mi instancia:
+EXEC sp_addlinkedserver 
+    @server = 'LocalAlias',
+    @srvproduct = '',
+    @provider = 'SQLNCLI',
+    @datasrc = 'localhost\SQLEXPRESS';
+
+EXEC sp_addlinkedsrvlogin 
+    @rmtsrvname = 'LocalAlias',
+    @useself = 'True',
+    @locallogin = NULL,
+    @rmtuser = NULL,
+    @rmtpassword = NULL;
+
+
+-- Pruebas
+SELECT * FROM LocalAlias.master.sys.databases;
+
+SELECT * FROM LocalAlias.Empresa_PRC.dbo.clientes;
+
+
+-- Para ejecutar EXEC AT: hay que habilitar RPC, y RPC_OUT
+EXEC master.dbo.sp_serveroption 
+    @server = 'LocalAlias', 
+    @optname = 'rpc', 
+    @optvalue = 'true';
+
+EXEC master.dbo.sp_serveroption 
+    @server = 'LocalAlias', 
+    @optname = 'rpc out', 
+    @optvalue = 'true';
+
+
+EXECUTE ('use Empresa_PRC; CREATE TABLE dbo.Test (Id INT)') AT LocalAlias;
+
+EXECUTE ('use Empresa_PRC; select * from pedidos') AT LocalAlias;
+
+EXEC master.dbo.sp_helpserver 'LocalAlias';
