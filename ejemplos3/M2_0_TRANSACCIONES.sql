104b77eecec6fec259fbf90c41d6f52fcb6eb5c0
aldebarran22 <antonioespinherranz@hotmail.com>
2026-02-10 15:48:36 +0100

    98

--- ejemplos3/M2_0_TRANSACCIONES.sql
+++ ejemplos3/M2_0_TRANSACCIONES.sql
@@ -20,3 +20,66 @@ CREATE TABLE dbo.TestLock (
 );
 
 INSERT INTO dbo.TestLock VALUES (1, 'Inicial');
+
+
+SELECT
+request_session_id AS Sesion,
+resource_type AS Recurso,
+resource_description AS Descripcion,
+request_mode AS Modo,
+request_status AS Estado
+FROM sys.dm_tran_locks
+ORDER BY request_session_id;
+
+
+SELECT
+blocking_session_id AS Bloqueador,
+session_id AS Bloqueado,
+wait_type,
+wait_time,
+wait_resource
+FROM sys.dm_exec_requests
+WHERE blocking_session_id<> 0;
+
+select * from sys.databases;
+
+SELECT
+OBJECT_NAME(p.object_id) AS Tabla,
+p.index_id,
+pa.allocated_page_page_id AS page_id,
+pa.allocated_page_file_id AS file_id
+FROM sys.partitions AS p
+JOIN sys.allocation_units AS au
+ON p.hobt_id = au.container_id
+JOIN sys.dm_db_database_page_allocations(8, NULL, NULL, NULL, 'DETAILED') AS pa
+ON au.allocation_unit_id = pa.allocation_unit_id;
+
+WHERE pa.allocated_page_page_id= 704
+AND pa.allocated_page_file_id= 1;
+
+
+select object_name(18099105);
+
+
+select * FROM sys.dm_tran_locks;
+
+select * FROM sys.dm_exec_requests where database_id=8;
+
+sp_who;
+sp_lock;
+
+DBCC TRACEON(3604);
+
+select object_name(72057594048479232);
+
+SELECT * FROM sys.dm_exec_requests WHERE session_id= 148;
+
+select * from sys.dm_tran_session_transactions;
+
+SELECT * FROM sys.dm_exec_requests WHERE session_id= 148;
+SELECT text FROM sys.dm_exec_sql_text(0x020000007BD4C30DAAB3CF89E033C4830597769E0EDDE30A0000000000000000000000000000000000000000);--, sys.dm_exec_requests r where r.session_id=148;
+
+select * from sys.dm_exec_requests;
+
+
+;
